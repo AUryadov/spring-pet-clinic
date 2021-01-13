e9f9edacf5c4ad3227a08462464a4e89ee4e70a8
Aleksandr Uryadov <aleksandr.uryadov@pashabank.az>
2021-01-13 13:50:54 +0400

    refactoring to common crud interface

--- pet-clinic-data/src/main/java/auryadov/springframework/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/auryadov/springframework/springpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package auryadov.springframework.springpetclinic.services;
 
 import auryadov.springframework.springpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
