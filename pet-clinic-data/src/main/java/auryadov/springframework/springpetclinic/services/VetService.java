361befb6643bd1e12cab4c1ec3adbb70ab732000
Aleksandr Uryadov <aleksandr.uryadov@pashabank.az>
2021-01-13 10:00:31 +0400

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/auryadov/springframework/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package auryadov.springframework.springpetclinic.services;
+
+import auryadov.springframework.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
