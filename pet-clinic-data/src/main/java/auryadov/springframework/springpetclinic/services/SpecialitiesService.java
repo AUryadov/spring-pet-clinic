ccf8994bef6f8b18c341afc2c4afabcfcb4cbebf
Aleksandr Uryadov <aleksandr.uryadov@pashabank.az>
2021-01-15 22:04:38 +0400

    added Specialities map impl

---
+++ pet-clinic-data/src/main/java/auryadov/springframework/springpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package auryadov.springframework.springpetclinic.services;
+
+import auryadov.springframework.springpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+}
