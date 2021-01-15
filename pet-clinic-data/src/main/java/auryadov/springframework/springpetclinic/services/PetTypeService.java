4466c114618b491b6834b8238e60bc34911eacbc
Aleksandr Uryadov <aleksandr.uryadov@pashabank.az>
2021-01-15 20:09:46 +0400

    added PetTypeService

---
+++ pet-clinic-data/src/main/java/auryadov/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package auryadov.springframework.springpetclinic.services;
+
+import auryadov.springframework.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
