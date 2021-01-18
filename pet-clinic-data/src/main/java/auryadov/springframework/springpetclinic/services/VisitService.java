68b7ae2628f9f5fa8e14627c1d68888c04f13a42
Aleksandr Uryadov <aleksandr.uryadov@pashabank.az>
2021-01-18 11:37:49 +0400

    implemented map for visit

---
+++ pet-clinic-data/src/main/java/auryadov/springframework/springpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package auryadov.springframework.springpetclinic.services;
+
+import auryadov.springframework.springpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long>{
+}
